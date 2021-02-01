ffe7c653028a44db09bfb340f6beff3938d08bc6
Tushe <tushe@mg-comm.com>
2021-02-01 21:07:00 +0200

    init payment card

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,5 +1,59 @@
 import React from 'react';
+import logo from '../assets/images/logo-tree.png'
+import {Button, Card, CardActions, CardContent, CardMedia, Grid, Typography} from '@material-ui/core';
+import {makeStyles} from '@material-ui/styles';
 
-const Payment = () => <h1>Welcome</h1>;
+const useStyles = makeStyles({
+  root: {
+    // maxWidth: 345,
+  },
+  media: {
+    height: 500,
+  },
+});
+
+const Payment = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{minHeight: '100vh'}}
+    >
+
+      <Grid item xs={3}>
+        <Card elevation={0} className={classes.root}>
+          <CardMedia
+            className={classes.media}
+            image={logo}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+    </Grid>
+  );
+};
 
 export default Payment;
