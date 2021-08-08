42f6866fe3676240b01d562745555df672714b77
Aleksey.y <alexey.y@jadexconsulting.com>
2021-08-08 22:03:57 +0300

    fix: update config for 'Pay User fee' on dev environment

--- src/shared/appconfig.js
+++ src/shared/appconfig.js
@@ -1,31 +1,29 @@
 const configLocal = {
-  VH_ORDER: "https://api.kli.one/pay",
-  PAYMENT_SUCCESS_URL: "http://9128465c414e.ngrok.io/success",
-  PAYMENT_CANCEL_URL: "https://localhost:3000/order",
-  PAYMENT_ERROR_URL: "https://localhost:3000/error"
-}
+  VH_ORDER: 'https://api.kli.one/pay',
+  PAYMENT_SUCCESS_URL: 'http://9128465c414e.ngrok.io/success',
+  PAYMENT_CANCEL_URL: 'https://localhost:3000/order',
+  PAYMENT_ERROR_URL: 'https://localhost:3000/error',
+};
 
 const configStaging = {
-  VH_ORDER: "https://api.vh-staging.2serv.eu/pay",
-  PAYMENT_SUCCESS_URL: "https://vh-staging.2serv.eu/success",
-  PAYMENT_CANCEL_URL: "https://vh-staging.2serv.eu/",
-  PAYMENT_ERROR_URL: "https://vh-staging.2serv.eu/error"
-}
+  VH_ORDER: 'http://api.eurokab.info/pay',
+  PAYMENT_SUCCESS_URL: 'https://api.eurokab.info/pay/success',
+  PAYMENT_CANCEL_URL: 'http://eurokab.info',
+  PAYMENT_ERROR_URL: 'https://api.eurokab.info/pay/error',
+};
 
 const configProd = {
-  VH_ORDER: "https://api.kli.one/pay",
-  PAYMENT_SUCCESS_URL: "https://kli.one/pay/success",
-  PAYMENT_CANCEL_URL: "https://kli.one/",
-  PAYMENT_ERROR_URL: "https://kli.one/pay/error"
-}
+  VH_ORDER: 'https://api.kli.one/pay',
+  PAYMENT_SUCCESS_URL: 'https://kli.one/pay/success',
+  PAYMENT_CANCEL_URL: 'https://kli.one/',
+  PAYMENT_ERROR_URL: 'https://kli.one/pay/error',
+};
 
-if (process.env.REACT_APP_LOCAL === "true") { 
-  module.exports = configLocal; 
-}
-
-else {
-  if (process.env.REACT_APP_STAGING === "true") { 
-    module.exports = configStaging; 
+if (process.env.REACT_APP_LOCAL === 'true') {
+  module.exports = configLocal;
+} else {
+  if (process.env.REACT_APP_STAGING === 'true') {
+    module.exports = configStaging;
   } else {
     module.exports = configProd;
   }
