2b70a9f19a973fbed3a62502e3c769e30d82dcde
Yash Shah <yashshh123@gmail.com>
2022-02-26 00:13:11 +0530

    Updated configuration changes
    Updated configuration changes

--- public/config/config-dev.js
+++ public/config/config-dev.js
@@ -1,13 +1,13 @@
 window.APP_CONFIG = {
-    VH_ORDER: 'https://api.eurokab.info/pay',
-  VH_SRV_PROFILE: 'https://api.eurokab.info/profile',
-  PAYMENT_SUCCESS_URL: 'http://eurokab.info/pay/success',
-  PAYMENT_CANCEL_URL: 'http://eurokab.info',
-  PAYMENT_ERROR_URL: 'http://eurokab.info/pay/error',
-  PAYMENT_DOMAIN: 'eurokab.info',
+  VH_ORDER: "https://api.eurokab.info/pay",
+  VH_SRV_PROFILE: "https://api.eurokab.info/profile",
+  PAYMENT_SUCCESS_URL: "http://eurokab.info/pay/success",
+  PAYMENT_CANCEL_URL: "http://eurokab.info",
+  PAYMENT_ERROR_URL: "http://eurokab.info/pay/error",
+  PAYMENT_DOMAIN: "eurokab.info",
   KEYCLOAK_CONFIG: {
     realm: "master",
     url: "https://auth.2serv.eu/auth/",
-    clientId: "membership_pay_dev"
-  }
+    clientId: "membership_pay_dev",
+  },
 };
