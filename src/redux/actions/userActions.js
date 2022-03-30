5fd0a70f3b314f5a747f0d604af62e94f8b91697
Yash Shah <yashshh123@gmail.com>
2022-03-30 13:06:57 +0530

    Added: API integration of User Profile, membership.
    Added API:
    Membership
    Profile
    
    Create Reducers:
    User Membership

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,8 +1,22 @@
-import {SET_LOGGED_IN_USER} from '../types';
+import { SET_LOGGED_IN_USER, SET_MEMBERSHIP_DATA, SET_USER_PROFILE_DATA } from "../types";
 
 export function setLoggedInUser(value) {
   return {
     type: SET_LOGGED_IN_USER,
-    payload: value
+    payload: value,
+  };
+}
+
+export function setMembershipData(value) {
+  return {
+    type: SET_MEMBERSHIP_DATA,
+    payload: value,
+  };
+}
+
+export function setUserProfileData(value) {
+  return {
+    type : SET_USER_PROFILE_DATA,
+    payload : value
   }
 }
--- src/redux/reducers/userReducers.js
+++ src/redux/reducers/userReducers.js
@@ -1,23 +1,36 @@
-import {SET_LOGGED_IN_USER} from '../types';
+import { SET_LOGGED_IN_USER, SET_MEMBERSHIP_DATA, SET_USER_PROFILE_DATA } from "../types";
 
 const DEFAULT = {
   authenticated: false,
   keycloak: null,
+  membershipdata: undefined,
+  profileData: undefined,
   profile: {
-    email: '',
-    firstName: '',
-    lastName: '',
-    username: ''
-  }
+    email: "",
+    firstName: "",
+    lastName: "",
+    username: "",
+  },
 };
 
-export default function reducer(state= DEFAULT, {type, payload}) {
+export default function reducer(state = DEFAULT, { type, payload }) {
   switch (type) {
     case SET_LOGGED_IN_USER:
       return {
         ...state,
-        profile: {...state.profile},
-        ...payload
+        profile: { ...state.profile },
+        ...payload,
+      };
+    case SET_MEMBERSHIP_DATA:
+      return {
+        ...state,
+        membershipdata: payload,
+      };
+
+    case SET_USER_PROFILE_DATA:
+      return {
+        ...state,
+        profileData: payload,
       };
     default:
       return state;
